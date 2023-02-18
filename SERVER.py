b3fcefcc2773504a4235acb77b96da9f4fff897d
Toyok1 <39643879+Toyok1@users.noreply.github.com>
2023-02-18 18:27:05 +0100

    feat: created branch

--- SERVER.py
+++ SERVER.py
@@ -8,17 +8,106 @@ import chat_pb2_grpc
 
 class ChatServer(chat_pb2_grpc.ChatServiceServicer):
     def __init__(self):
+<<<<<<< Updated upstream
         self.clients = []
+=======
+        # List with all the chat history
+        self.chats = []
+        self.hp = []
+        self.turns = []
+        self.listUser = [] #  every element is a dictionary with  {"name":  username,  "ip": user public ip , "ping_time": timestamp of last pingpong , "role": Bool}
+        self.fernet = Fernet(key)   
+        threading.Thread(target=self.__clean_user_list, daemon=True).start()
+        self.activeMap = chat.Map()
+        self.activeMap.board = ""
+        self.isStartedGame = False
+>>>>>>> Stashed changes
 
     def Connect(self, request, context):
         print(f"{request.name} has connected")
         self.clients.append(request.name)
         return chat_pb2.ConnectResponse(message=f"Welcome, {request.name}!")
 
+<<<<<<< Updated upstream
     def Disconnect(self, request, context):
         print(f"{request.name} has disconnected")
         self.clients.remove(request.name)
         return chat_pb2.DisconnectResponse(message=f"Goodbye, {request.name}!")
+=======
+    def  __updateUserList(self, req_ip):
+        for  utente  in  self.listUser:
+            if  utente["ip"]  ==  req_ip: 
+                utente["ping_time"]  =  time.time() 
+    
+    '''def __createBoard(self):
+        self.activeMap.length = random.randint(40,90)
+        self.activeMap.board =  str(random.choices(range(0,  8), k = self.activeMap.length))'''
+
+    def __distributeHealth(self):
+        random.choice(self.listUser)["role"] = True
+        for user in self.listUser:
+            if user["role"]:
+                self.hp.append({"ip": user["ip"], "hp": 100, "user": user["name"]}) #TODO tweak values
+            else:
+                self.hp.append({"ip": user["ip"], "hp": 50, "user": user["name"]}) #TODO tweak values
+        
+        length = len(self.hp)
+
+        values = []
+        values2 = []
+        for d in self.hp:
+                values.append(d["user"])
+        for d in self.hp:
+                values2.append(d["hp"])
+
+        board = str(values) + str(values2)
+        mmmap = chat.Map()
+        mmmap.length = length
+        mmmap.board = board
+        self.activeMap = mmmap
+    
+    # The stream which will be used to send new messages to clients
+    def ChatStream(self, request_iterator, context):
+        """
+        This is a response-stream type call. This means the server can keep sending messages
+        Every client opens this connection and waits for server to send new messages
+        :param request_iterator:
+        :param context:
+        :return:
+        """
+        lastindex = 0
+        # For every client a infinite loop starts (in gRPC's own managed thread)
+        while True:
+            # Check if there are any new messages
+            while len(self.chats) > lastindex:
+                n = self.chats[lastindex]
+                lastindex += 1
+                yield n
+
+    def SendHealth(self, request: chat.Health, context):
+        new_h = {"ip": self.fernet.decrypt(request.ip).decode(), "hp": request.hp}
+        self.hp.append(new_h)
+        return chat.Empty()
+    
+    def HealthStream(self, request_iterator, context):
+        """
+        This is a response-stream type call. This means the server can keep sending messages
+        Every client opens this connection and waits for server to send new messages
+        :param request_iterator:
+        :param context:
+        :return:
+        """
+        lastindex = 0
+        # For every client a infinite loop starts (in gRPC's own managed thread)
+        while True:
+            # Check if there are any new messages
+            while len(self.hp) > lastindex:
+                n = chat.Health()
+                n.ip = self.fernet.encrypt(self.hp[lastindex]["ip"].encode())
+                n.hp = self.hp[lastindex]["hp"]
+                lastindex += 1
+                yield n
+>>>>>>> Stashed changes
 
     def SendMessage(self, request, context):
         print(f"{request.name}: {request.message}")
@@ -26,10 +115,93 @@ class ChatServer(chat_pb2_grpc.ChatServiceServicer):
             if client != request.name:
                 yield chat_pb2.SendMessageResponse(name=request.name, message=request.message)
 
+<<<<<<< Updated upstream
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatServer(), server)
     server.add_insecure_port("[::]:50051")
+=======
+    def SendPrivateInfo(self, request: chat.PrivateInfo, context):
+        """
+        This method is called when a clients sends a PrivateInfo to the server.
+        :param request:
+        :param context:
+        :return:
+        """
+        encMessage = request.ip
+        user = request.user
+        decMessage = self.fernet.decrypt(encMessage).decode()
+        
+        new_user  =  {"name":  user,  "ip":  decMessage,  "ping_time":  time.time(), "role": False}
+        if len(self.listUser) == 0:
+            self.turns.append(request)
+        self.listUser.append(new_user)
+        return chat.Empty()  # something needs to be returned required by protobuf language, we just return empty msg
+
+    def SendPing(self, request: chat.Ping, context):
+        """ Fulfills SendPing RPC defined in ping.proto """
+        self.__updateUserList(self.fernet.decrypt(request.ip).decode()) 
+        return chat.Pong(message="Thanks, friend!")
+    
+    def EndTurn(self, request: chat.PrivateInfo, context):
+        ipLast = self.fernet.decrypt(request.ip).decode()
+        userLast = request.user
+        ipNext = self.listUser[0]["ip"] #if i can't find the ip we default to the first person in the list
+        userNext = self.listUser[0]["ip"]
+
+        for i in range(0,len(self.listUser)):
+            #if self.listUser[i]["ip"] == ipLast:
+            if userLast == self.listUser[i]["name"]:
+                ipNext = self.listUser[(i+1)%len(self.listUser)]["ip"]
+                userNext = self.listUser[(i+1)%len(self.listUser)]["name"]
+        r = chat.PrivateInfo() #create the return message, encrypt the ip and send it in broadcast to everyone.
+        r.ip = self.fernet.encrypt(ipNext.encode()) #TODO: create the methods to recieve the broadcast
+        r.user = userNext
+        self.turns.append(r)
+
+        return chat.Empty()
+
+    def TurnStream(self, request_iterator,context):
+        lastindex = 0
+        while True:
+            while len(self.turns) > lastindex:
+                n = self.turns[lastindex]
+                lastindex += 1
+                yield n
+                
+    def GetActiveMap(self, request_iterator, context):
+        """ Create game board and send to client, if map is none client resend request"""
+        return self.activeMap
+
+    
+    def StartGame(self, request: chat.PrivateInfo, context):
+        print('StartGame', self.listUser)
+        ip = self.fernet.decrypt(request.ip).decode()
+        #if self.listUser[0]["ip"] == ip:    #TODO quando si testa su macchine diverse usare l'ip
+        if self.listUser[0]["name"] == request.user:
+            print("L'host è onnipotente")
+            self.isStartedGame = True
+            self.__distributeHealth()
+            #self.__createBoard()
+        return self.activeMap
+    
+    def FinishGame(self, request_iterator, context):
+        self.isStartedGame = False
+        #self.activeMap.board = None
+        n = chat.Note()
+        n.message = "OK" 
+        return n
+
+if __name__ == '__main__':
+    port = 11912  # a random port for the server to run on
+    # the workers is like the amount of threads that can be opened at the same time, when there are 10 clients connected
+    # then no more clients able to connect to the server.
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))  # create a gRPC server
+    rpc.add_ChatServerServicer_to_server(ChatServer(), server)  # register the server to gRPC
+    # gRPC basically manages all the threading and server responding logic, which is perfect!
+    print('Starting server. Listening...')
+    server.add_insecure_port('[::]:' + str(port))
+>>>>>>> Stashed changes
     server.start()
     try:
         while True:
---
+++ client.py
@@ -0,0 +1,163 @@
+import threading
+
+from tkinter import *
+from tkinter import simpledialog
+
+import grpc
+import time
+import random
+
+import chat_pb2 as chat
+import chat_pb2_grpc as rpc
+
+from requests import get
+from cryptography.fernet import Fernet
+
+#address = 'localhost'
+port = 11912 # decide if change this port
+key = b'ZhDach4lH7NbH-Gy9EfN2e2HNrWRfbBFD8zeCTBgdEA='
+
+class Client:
+
+    def __init__(self, u: str, a: str, window):
+        # the frame to put ui components on
+        self.window = window
+        self.username = u
+        self.map = chat.Map()
+        self.map.length = 0
+        self.listHealth = []
+        #self.map.length = 0
+        self.myTurn = False
+
+        self.ip = get('https://api.ipify.org').content.decode('utf8')
+        self.fernet = Fernet(key)
+        encMessage = self.fernet.encrypt(self.ip.encode())
+        
+        # create a gRPC channel + stub
+        channel = grpc.insecure_channel(a + ':' + str(port))
+        self.conn = rpc.ChatServerStub(channel)
+
+        threading.Thread(target=self.__listen_for_pingpong, daemon=True).start()
+        
+           
+        self.p = chat.PrivateInfo()  # create protobug message (called Note)
+        self.p.ip = encMessage
+        self.p.user = u
+        self.conn.SendPrivateInfo(self.p)  # send the Note to the server
+        self.__setup_ui()
+
+        threading.Thread(target=self.__listen_for_turns, daemon=True).start()
+        threading.Thread(target=self.__listen_for_health, daemon=True).start()
+
+        if self.p.user != 'a': # host TODO change this line to be "not the host" (probably easiest when we launch both programs from the same place)
+            while self.map.length == 0:
+                resp = self.conn.GetActiveMap(chat.Empty())
+                if resp.board != "":
+                    self.map = resp
+                    self.cleanMap(self.map)
+                    time.sleep(2)
+
+        self.window.mainloop()
+        
+        
+    def __listen_for_pingpong(self):
+        while True:
+            p = chat.Ping()  # create protobug message (called Note)
+            p.ip = self.fernet.encrypt(self.ip.encode())
+            pong = self.conn.SendPing(p)
+            time.sleep(5)
+            #pong.message None chiudi gioco
+
+
+    def __listen_for_health(self):
+        """
+        This method will be ran in a separate thread as the main/ui thread, because the for-in call is blocking
+        when waiting for new messages
+        """
+        for health in self.conn.HealthStream(chat.Empty()): # this line will wait for new messages from the server!
+            req_ip = self.fernet.decrypt(health.ip).decode()  
+            print("R[{}] {}".format(health.user, health.hp)) 
+            for  utente  in  self.listHealth:
+                #if  utente["ip"]  ==  req_ip: 
+                if  utente["name"]  ==  health.user:
+                    utente["hp"]  =  health.hp
+
+    def __listen_for_turns(self):
+        time.sleep(0.5) #some wait time before running to ensure that the ui gets created in time 
+        for turn in self.conn.TurnStream(chat.Empty()):
+            if turn.user == self.username: #for testing use this line
+            #if self.fernet.decrypt(turn.ip).decode() == self.ip:   
+                print("Mio turno: " + self.username)
+                self.turn_button["state"] = "normal"
+                #TODO start the turn
+
+    def send_end_turn(self):
+        info = chat.PrivateInfo()
+        info.ip = self.fernet.encrypt(self.ip.encode())
+        info.user = self.username
+        print("Ending my turn: " + self.username)
+        self.turn_button["state"] = "disabled"
+        n = chat.Health()
+        n.ip = info.ip
+        n.hp = random.randint(0, 100)
+        n.user = self.username
+        self.conn.SendHealth(n)
+        print(self.listHealth)
+        self.conn.EndTurn(info)
+
+    def cleanMap(self, mess):
+        string = mess.board
+        rows = string.split("][")  # split the string by the '][' delimiter
+        array = []
+        for row in rows:
+            row = row.strip("[]")  # remove the square brackets from the row string
+            values = [str(x) for x in row.split(",")]  # split the row string by the ',' delimiter and convert the values to integers
+            array.append(values)
+            
+        self.listHealth.extend([{"ip":self.ip, "hp":array[1][i], "user":array[0][i]} for i in range(0,len(array[0]))])
+
+    def send_message(self, event):
+        """
+        This method is called when user enters something into the textbox
+        """
+        message = self.entry_message.get()  # retrieve message from the UI
+        if message != '':
+            if message == '1': # fake starting
+                mess = self.conn.StartGame(self.p)
+                self.cleanMap(mess)
+            
+            n = chat.Note()  # create protobug message (called Note)
+            n.name = self.username  # set the username
+            n.message = message  # set the actual message of the note
+            print("S[{}] {}".format(n.name, n.message))  # debugging statement
+            self.conn.SendNote(n)  # send the Note to the server
+
+    def __setup_ui(self):
+        self.chat_list = Text()
+        self.chat_list.pack(side=TOP)
+        self.lbl_username = Label(self.window, text=self.username)
+        self.lbl_username.pack(side=LEFT)
+        self.entry_message = Entry(self.window, bd=5)
+        self.entry_message.bind('<Return>', self.send_message)
+        self.entry_message.focus()
+        self.entry_message.pack(side=BOTTOM)
+        self.turn_button = Button(self.window, text = "End Turn", command = self.send_end_turn)
+        self.turn_button.pack()
+        self.turn_button["state"] = "disabled"
+
+
+if __name__ == '__main__':
+    root = Tk()  # I just used a very simple Tk window for the chat UI, this can be replaced by anything
+    frame = Frame(root, width=300, height=300)
+    frame.pack()
+    root.withdraw()
+    username = None
+    while username is None:
+        # retrieve a username so we can distinguish all the different clients
+        username = simpledialog.askstring("Username", "What's your username?", parent=root)
+    address = "localhost" #None
+    #while address is None:
+        # retrieve a username so we can distinguish all the different clients
+    #    address = simpledialog.askstring("Game's address", "What's the address?", parent=root)
+    root.deiconify()  # don't remember why this was needed anymore...
+    c = Client(username, address, frame)  # this starts a client and thus a thread which keeps connection to server open
