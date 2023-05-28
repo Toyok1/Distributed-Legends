55ddd6d8aa1dcf827f5539462facd412c91a5776
DavideTalevi98 <davidetalevi98@gmail.com>
2023-05-28 13:25:15 +0200

    feat(server_auth): add lobby dictionary

--- Lobby.py
+++ Lobby.py
@@ -20,9 +20,58 @@ class Lobby(lobby_auth_rpc.LobbyAuthServer):
         self.fernet = Fernet(key)
         self.listUser = []
         self.start_stream = []
-        # TODO: add dictionary of getUid for lobbyId and List of this player
+        
+        self.list_of_lobby = []
+        
+        self.list_of_lobby.append({'id_lobby': 3,'listPlayers':[{'ip':'127.0.0.1', 'unique_id':12, 'username':'Davide', 'user_type': 2, 'ping_time':'32443453534'}]})
+        
+        print(self.list_of_lobby)
+        
+    def _get_lobby(self, lobby_id):
+        for lobby in self.list_of_lobby:
+            if lobby['id_lobby'] == lobby_id:
+                return lobby
+        return None
+    
+    def _get_players_by_lobby_id(self, lobby_id):
+        for lobby in self.list_of_lobby:
+            if lobby['id_lobby'] == lobby_id:
+                return lobby['listPlayers']
+        return None
 
+    def _add_player_to_lobby(self, lobby_id,player):
+        for lobby in self.list_of_lobby:
+            if lobby['id_lobby'] == lobby_id:
+                lobby['listPlayers'].append(player)
+                return True
+        return False
+
+    def _get_my_lobby_by_player_id(self, player_id):
+        for lobby in self.list_of_lobby:
+            for player in lobby['listPlayers']:
+                if player['unique_id'] == player_id:
+                    return lobby
+        return None
+
+    def _get_player_ip(self, player_ip):
+        encMessage = player_ip
+        return self.fernet.decrypt(encMessage).decode()
+        
+
+    # controlla se esiste la lobby con id_lobby == request.id_lobby e se esiste aggiungi il nuovo player altrimenti aggiungi a list_of_lobby un nuovo oggetto con id_lobby = equest.id_lobby e aggiungi il new_user alla corrispettiva lista listplayer
     def SendPrivateInfo(self, request: lobby_auth.PrivateInfo, context):
+        ip = self._get_player_ip(request.ip)
+        user = request.user
+        u_id = request.u_id
+        user_type = request.user_type
+        new_user = player.Player(ip=ip, unique_id=u_id, username=user, user_type=user_type, ping_time=time.time())
+        if _get_lobby(request.id_lobby) == None:
+            self.list_of_lobby.append({'id_lobby': request.id_lobby,'listPlayers':[new_user]})
+        else:
+            self._add_player_to_lobby(request.id_lobby,new_user)
+        return lobby_auth.Empty_Lobby()
+
+    '''def SendPrivateInfo(self, request: lobby_auth.PrivateInfo, context):
         encMessage = request.ip
         user = request.user
         decMessage = self.fernet.decrypt(encMessage).decode()
@@ -38,7 +87,7 @@ class Lobby(lobby_auth_rpc.LobbyAuthServer):
             print("User already in the game or game already started")
         print("Player", self.listUser)
         # self.text_area.config(text='\n'.join([name.getUsername() for name in self.listUser]))
-        return lobby_auth.Empty_Lobby()
+        return lobby_auth.Empty_Lobby()'''
 
     def StartedStream(self, request, context):
         lastindex = 0
@@ -49,7 +98,10 @@ class Lobby(lobby_auth_rpc.LobbyAuthServer):
                 yield n
 
     def StartGame(self, request: lobby_auth.PrivateInfo, context):
-        self.start_stream.append(lobby_auth.StartedBool(name="game started", mesage=player.tranformFullListIntoJSON(self.listUser)))
+        #myLobby = list(filter(lambda lobby: self.list_of_lobby['id_lobby'] == request.id_lobby, self.list_of_lobby))
+        myLobby = self._get_players_by_lobby_id(request.id_lobby)
+        print(myLobby)
+        self.start_stream.append(lobby_auth.StartedBool(name="game started", mesage=player.tranformFullListIntoJSON(myLobby)))
         time.sleep(10)
         os.kill(self.pId, signal.SIGTERM)
         return lobby_auth.Empty_Lobby()
--- lobby_auth_pb2.py
+++ lobby_auth_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10lobby_auth.proto\x12\x04grpc\"\r\n\x0b\x45mpty_Lobby\"%\n\x04Note\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"H\n\x0bPrivateInfo\x12\n\n\x02ip\x18\x01 \x01(\x0c\x12\x0c\n\x04user\x18\x02 \x01(\t\x12\x0c\n\x04u_id\x18\x03 \x01(\t\x12\x11\n\tuser_type\x18\x04 \x01(\x05\x32\xaf\x01\n\x0fLobbyAuthServer\x12\x37\n\x0fSendPrivateInfo\x12\x11.grpc.PrivateInfo\x1a\x11.grpc.Empty_Lobby\x12\x31\n\tStartGame\x12\x11.grpc.PrivateInfo\x1a\x11.grpc.Empty_Lobby\x12\x30\n\rStartedStream\x12\x11.grpc.Empty_Lobby\x1a\n.grpc.Note0\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10lobby_auth.proto\x12\x04grpc\"\r\n\x0b\x45mpty_Lobby\"%\n\x04Note\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"Z\n\x0bPrivateInfo\x12\n\n\x02ip\x18\x01 \x01(\x0c\x12\x0c\n\x04user\x18\x02 \x01(\t\x12\x0c\n\x04u_id\x18\x03 \x01(\t\x12\x11\n\tuser_type\x18\x04 \x01(\x05\x12\x10\n\x08id_lobby\x18\x05 \x01(\t2\xaf\x01\n\x0fLobbyAuthServer\x12\x37\n\x0fSendPrivateInfo\x12\x11.grpc.PrivateInfo\x1a\x11.grpc.Empty_Lobby\x12\x31\n\tStartGame\x12\x11.grpc.PrivateInfo\x1a\x11.grpc.Empty_Lobby\x12\x30\n\rStartedStream\x12\x11.grpc.Empty_Lobby\x1a\n.grpc.Note0\x01\x62\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'lobby_auth_pb2', globals())
@@ -25,7 +25,7 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _NOTE._serialized_start=41
   _NOTE._serialized_end=78
   _PRIVATEINFO._serialized_start=80
-  _PRIVATEINFO._serialized_end=152
-  _LOBBYAUTHSERVER._serialized_start=155
-  _LOBBYAUTHSERVER._serialized_end=330
+  _PRIVATEINFO._serialized_end=170
+  _LOBBYAUTHSERVER._serialized_start=173
+  _LOBBYAUTHSERVER._serialized_end=348
 # @@protoc_insertion_point(module_scope)
